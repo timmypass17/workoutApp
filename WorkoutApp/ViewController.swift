8ee1dd695cef9298913a21319608e44c4b28eeca
Timmy Nguyen <timmypass17@gmail.com>
2023-12-31 12:31:58 -0800

    Initial Commit

---
+++ WorkoutApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkoutApp
+//
+//  Created by Timmy Nguyen on 12/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
